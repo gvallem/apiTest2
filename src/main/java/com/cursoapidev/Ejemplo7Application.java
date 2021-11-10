5e1fedff4b4c80881e2233de8d8f03329e47551e
Guillermo Valle <gvamen@hotmail.com>
2021-11-09 21:51:23 -0600

    Initial commit
    Initial commit

---
+++ src/main/java/com/cursoapidev/Ejemplo7Application.java
@@ -0,0 +1,13 @@
+package com.cursoapidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejemplo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejemplo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoapidev/Ejemplo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoapidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
